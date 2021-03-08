116974767f160c4e62e5c517cd36a0a0e3282961
CherishICC <cherishinampudi@gmail.com>
2021-03-09 00:21:07 +0530

    edited src test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,16 @@
-import java.lang.Math;
-public class CalculatorTest 
-{}
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+public class CalculatorTest {
+	@Test
+	public void test_sqrt()
+	{
+		Calculator c = new Calculator();
+		double x = c.sqrt(25);
+		assertEquals(5.0,n,0.0);
+		x = c.sqrt(3);
+		assertEquals(1.7320508075688772,n,0.0);
+	}
+}
