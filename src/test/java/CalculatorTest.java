e31dfb3abc2eb7ac7860ba6b10156a6be2ba36e7
CherishICC <cherishinampudi@gmail.com>
2021-03-16 21:25:04 +0530

    added additional test cases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,35 +11,49 @@ public class CalculatorTest {
 		assertEquals(5.0,x,0.0);
 		x = c.sqrt(3);
 		assertEquals(1.7320508075688772,x,0.0);
+		x = c.sqrt(-3);
+		assertEquals(Double.NaN,x,0.0);
 	}
 
 	@Test
 	public void testFactorial()
 	{
 		Calculator c = new Calculator();
-		double n = c.factorial(0);
-		assertEquals(1,n,0.0);
-		n = c.factorial(5);
-		assertEquals(120,n,0.0);
+		double x = c.factorial(0);
+		assertEquals(1,x,0.0);
+		x = c.factorial(5);
+		assertEquals(120.0,x,0.0);
+		x = c.factorial(1);
+		assertEquals(1.0,x,0.0);
+		x = c.factorial(-5);
+		assertEquals(Double.NaN,x,0.0);
 	}
 
 	@Test
 	public void testLogarithm()
 	{
 		Calculator c = new Calculator();
-		double n = c.ln(1);
-		assertEquals(0.0,n,0.0);
-		n = c.ln(4);
-		assertEquals(1.3862943611198906,n,0.001);
+		double x = c.ln(1);
+		assertEquals(0.0,x,0.0);
+		x = c.ln(4);
+		assertEquals(1.3862943611198906,x,0.001);
+		x = c.ln(0);
+		assertEquals(Double.NaN,x,0.001);
+		x = c.ln(10);
+		assertEquals(2.302585092994046,x,0.001);
 	}
 
 	@Test
 	public void testPower()
 	{
 		Calculator c = new Calculator();
-		double n = c.power(2,3);
-		assertEquals(8.0,n,0.0);
-		n = c.power(-1,1);
-		assertEquals(-1.0,n,0.0);
+		double x = c.power(2,3);
+		assertEquals(8.0,x,0.0);
+		x = c.power(-1,1);
+		assertEquals(-1.0,x,0.0);
+		x = c.power(0,1);
+		assertEquals(0.0,x,0.0);
+		x = c.power(-1,-1);
+		assertEquals(-1.0,x,0.0);
 	}
 }
